f0266e1428d1325937567df01ce49129ed8e27c9
Emily Nguyen <emily.linh.nguyen@gmail.com>
2022-03-23 14:32:24 -0400

    all functionality - delete, edit, view

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views import generic
+from django.urls import reverse_lazy
+
 from .models import Task
 
 class TaskList(generic.ListView):
@@ -14,3 +17,22 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'   # your own name for the list as a template variable
     template_name = 'base/task.html'  # Specify your own template name/location
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks') #go back to list
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks') #go back to list
+
+
+class DeleteView(DeleteView):
+    model = Task
+    fields = '__all__'
+    context_object_name = 'task'   # your own name for the list as a template variable
+    success_url = reverse_lazy('tasks') #go back to list
+
+
