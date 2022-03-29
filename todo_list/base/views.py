38d395aaadb7688b529a65f9f6a8a6207c4e3093
Emily Nguyen <emily.linh.nguyen@gmail.com>
2022-03-29 13:35:13 -0400

    styling

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -73,7 +73,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super(taskCreate, self).form_valid(form)
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
